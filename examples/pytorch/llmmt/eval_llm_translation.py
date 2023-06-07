03049fdec8253c0dbee23ea6047c42d9ca6fee4c
Haoran Xu <t-haoranxu@microsoft.com>
2023-06-07 16:46:43 -0700

    truncate 256 + adjust mpt and faclon

--- examples/pytorch/llmmt/eval_llm_translation.py
+++ examples/pytorch/llmmt/eval_llm_translation.py
@@ -1,4 +1,5 @@
 from transformers import AutoModelForCausalLM, AutoTokenizer, set_seed
+from transformers import LlamaTokenizer
 import torch
 from tqdm import tqdm
 import argparse
@@ -55,8 +56,22 @@ def clean_outputstring(output, key_word):
 
 def generate(args):
     # Load model and tokenizer, note that the fast tokenizer currently does not work correctly
-    model = AutoModelForCausalLM.from_pretrained(args.model_name, torch_dtype=torch.float16).cuda()
-    tokenizer = AutoTokenizer.from_pretrained(args.model_name, use_fast=False, padding_side='left')
+    if "llama" in args.model_name:
+        tokenizer = LlamaTokenizer.from_pretrained(args.model_name, use_fast=False)
+    elif "mpt" in args.model_name:
+        ## GPTNeoXTokenizer only supports for AutoTokenizerFast
+        tokenizer = AutoTokenizer.from_pretrained(args.model_name, use_fast=True, padding_side='left')
+    else:
+        tokenizer = AutoTokenizer.from_pretrained(args.model_name, use_fast=False, padding_side='left')
+
+    if "llama" in args.model_name or "falcon" in args.model_name or "mpt" in args.model_name:
+        is_padding = False # llama and falcon does not have padding_id
+        args.batch_size = 1 # and force batch size to be 1
+        tokenizer.add_special_tokens({'pad_token': '[PAD]'})
+    else:
+        is_padding = True
+    # print(tokenizerd("test", return_tensors="pt", padding=is_padding))
+    model = AutoModelForCausalLM.from_pretrained(args.model_name, torch_dtype=torch.float16, trust_remote_code=True).cuda()
 
     src_fullname = LANG_TABLE[args.src_lang]
     tgt_fullname = LANG_TABLE[args.tgt_lang]
@@ -72,8 +87,10 @@ def generate(args):
     # Generate
     cleaned_outputs = []
     for eval_batch in tqdm(eval_data):
-        input_ids = tokenizer(eval_batch, return_tensors="pt", padding=True).input_ids.cuda()
+        input_ids = tokenizer(eval_batch, return_tensors="pt", padding=is_padding).input_ids.cuda()
         max_length = min(int(input_ids.shape[-1] * 2.5), args.max_token_in_seq)
+        # truncate the input length
+        input_ids = input_ids[:, :int(args.max_token_in_seq/2)]
         generated_ids = model.generate(input_ids, num_beams=args.beam_size, length_penalty=1, max_length=max_length, do_sample=True, top_k=50)
         outputs = tokenizer.batch_decode(generated_ids, skip_special_tokens=True)
         for out in outputs:
--- examples/pytorch/llmmt/eval_llm_translation.sh
+++ examples/pytorch/llmmt/eval_llm_translation.sh
@@ -1,15 +1,30 @@
 model=${1:-"facebook/opt-6.7b"}
 src=${2:-de}
 tgt=${3:-en}
-bz=${4:-16}
-beam=${5:-1}
+bz=${4:-4}
+beam=${5:-5}
+gpu=${6:-0}
+maxtoken=${7:-512}
 
+# model: 
+# opt-6.7b: facebook/opt-6.7b; covering languages: UNKOWN
+#           overlapping: UNKOWN
+# llama-7b: decapoda-research/llama-7b-hf #covering language:bg, ca, cs, da, de, en, es, fr, hr, hu, it, nl, pl, pt, ro, ru, sl, sr, sv, uk.
+#           overlapping: cs, de, fr, en, ru, uk, 
+# falcon-7b: tiiuae/falcon-7b
+# BLOOM-7b: bigscience/bloom-7b1
+# mpt-7b: mosaicml/mpt-7b
+
+source activate llmmt
+
+echo "model ${model} src ${src} tgt ${tgt} beam ${beam} bz ${bz} gpu ${gpu}"
 eval_path=/home/aiscuser/gpt-MT/evaluation/testset/wmt-testset/${src}${tgt}/test.${src}-${tgt}.${src}
 gold_path=/home/aiscuser/gpt-MT/evaluation/testset/wmt-testset/${src}${tgt}/test.${src}-${tgt}.${tgt}
 output_path=/mnt/sdrgmainz01wus2/t-haoranxu/results/zero-shot-llmmt-beam${beam}/${model}/${src}${tgt}/
 mkdir -p ${output_path}
 output_path=${output_path}test.${src}-${tgt}.${tgt}
 
+export CUDA_VISIBLE_DEVICES=${gpu}
 python eval_llm_translation.py \
 --model_name ${model} \
 --eval_path ${eval_path} \
@@ -19,9 +34,12 @@ python eval_llm_translation.py \
 --beam_size ${beam} \
 --batch_size ${bz} \
 --seed 42 \
---max_token_in_seq 512 \
+--max_token_in_seq ${maxtoken} \
 
 
 SACREBLEU_FORMAT=text sacrebleu -tok flores200 -w 2 ${output_path} < ${gold_path} > ${output_path}.bleu
+comet-score -s ${eval_path} -t ${output_path} -r ${gold_path} > ${output_path}.comet
 
 cat ${output_path}.bleu
+tail -n 1 ${output_path}.comet
+
