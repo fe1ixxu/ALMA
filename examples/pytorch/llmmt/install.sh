96b8bf565548fb56b4c20eea6cde218af0529f1c
Haoran Xu <45837851+fe1ixxu@users.noreply.github.com>
2023-07-25 12:14:39 -0700

    Create install.sh

---
+++ examples/pytorch/llmmt/install.sh
@@ -0,0 +1,35 @@
+## copy required repo
+# cd home
+cd ../../../../
+git clone https://github.com/microsoft/gpt-MT.git
+git clone https://github.com/huggingface/peft.git
+echo "Copying data from /mnt/sdrgmainz01wus2/t-haoranxu/filtered_wmt22/...."
+cp -r /mnt/sdrgmainz01wus2/t-haoranxu/filtered_wmt22/ .
+echo "Copying finished"
+# install virtual env
+conda create -n llmmt python=3.8
+conda activate llmmt
+pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/rocm5.4.2
+cd LLMMT; pip install -e ./; cd ..
+cd peft; pip install -e ./; cd ..
+pip install sentencepiece
+pip install sacrebleu
+pip install sacrebleu[ja]
+pip install ipython
+pip install pytest
+pip install datasets
+pip install evaluate
+pip3 install deepspeed
+pip install einops
+pip install wandb
+pip install zstandard
+pip install accelerate==0.20.3
+
+# install eval env
+conda create -n comet python=3.8
+conda activate comet
+pip install unbabel-comet
+pip install sacrebleu[ja]
+conda deactivate
+# cd workplace
+cd ./LLMMT/examples/pytorch/llmmt
