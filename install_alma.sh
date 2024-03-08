937b9e01dfccb1059dbd50bdaf8de4ec904c0110
Haoran Xu <45837851+fe1ixxu@users.noreply.github.com>
2024-03-08 15:45:23 +0800

    update accelerate version

--- install_alma.sh
+++ install_alma.sh
@@ -9,7 +9,7 @@ pip3 install deepspeed==0.11.1
 pip install einops
 pip install wandb
 pip install zstandard
-pip install accelerate==0.24.0
+pip install accelerate==0.27.2
 pip install jsonlines
 pip install git+https://github.com/fe1ixxu/ALMA.git@alma-r-trl
 pip install unbabel-comet
